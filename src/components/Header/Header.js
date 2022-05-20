f3f59d94f380743a0d78fed56b5a3d491d79ba68
Abu Naem <88497497+abunaem123@users.noreply.github.com>
2022-05-20 15:17:58 +0600

    Header Complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import  logo from '../../Images/Logo.jpg'
 
 const Header = () => {
     const [user] = useAuthState(auth)
@@ -21,10 +22,10 @@ const Header = () => {
                         <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         {
                             user && <>
-                                <Nav.Link  as={Link} to="manageinventory">Manage Items</Nav.Link>
-                                <Nav.Link  as={Link} to="addmyitem">Add My Item</Nav.Link>
+                                <Nav.Link as={Link} to="manageinventory">Manage Items</Nav.Link>
+                                <Nav.Link as={Link} to="addmyitem">Add My Item</Nav.Link>
 
-                                <Nav.Link  as={Link} to="myitem">My Item</Nav.Link>
+                                <Nav.Link as={Link} to="myitem">My Item</Nav.Link>
                             </>
                         }
                     </Nav>
@@ -39,6 +40,42 @@ const Header = () => {
                                 Login
                             </Nav.Link>}
                 </Nav>
+            </Navbar>
+
+            <Navbar collapseOnSelect sticky='top' expand="lg" bg="black" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={50} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        {
+                            user && <>
+                                <Nav.Link as={Link} to="manageinventory">Manage Items</Nav.Link>
+                                <Nav.Link as={Link} to="addmyitem">Add My Item</Nav.Link>
+
+                                <Nav.Link as={Link} to="myitem">My Item</Nav.Link>
+                            </>
+                        }
+                        </Nav>
+                        <Nav>
+                        <Nav>
+                    {
+                        user ?
+                            <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign Out</button>
+                            :
+
+                            <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>}
+                </Nav>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
 
         </div>
